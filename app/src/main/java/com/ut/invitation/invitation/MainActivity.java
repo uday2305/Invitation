7aa137ba310a15efae353c64dc1b0c0fe22ecdf5
uday2305 <udaybhaskar2305@gmail.com>
2017-01-30 22:01:05 +0100

    Initial commit

---
+++ app/src/main/java/com/ut/invitation/invitation/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ut.invitation.invitation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
